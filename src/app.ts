dd3ace8a9ea8581fd0985375b32753183980e216
Diadem Grace Arroz <diademgracearroz@gmail.com>
2022-10-09 23:35:12 +0800

    express change

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,12 @@
-/*
- * I have problem in creating a command to execute the code
- */
-
 import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
-const port: number = 0888;
+const port: number = 3001;
 
 app.get('/pre', (req: Request, res: Response) => {
     res.send('Hello pre')
 })
 
 app.listen(port, function() {
-    console.log(`Ap is listening on port ${port} !`)
+    console.log(`App is listening on port ${port} !`)
 })
